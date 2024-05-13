9196362f541194776d0b2310c1e47fb5611cd820
Niji <nijikokun@gmail.com>
2024-05-12 19:25:08 -0700

    feat: update packages and improve logging

--- .build/build_run.js
+++ .build/build_run.js
@@ -3,19 +3,26 @@ import {
   runWebpackDev,
   runTweego,
   moveFiles,
+  getBuildSpinner
 }  from "./build_commands.js";
 
-(async () => {
+(async () => {  
+  const spinner = getBuildSpinner(true);
+  spinner.start(`Running build.`);
+
   // Verify tweego is installed
   await verifyTweegoInstall();
-
+  
   // Run webpack
   let [err, res] = await runWebpackDev();
   if (err) throw new Error(err);
-
+  
   // Move files
   await moveFiles();
-
+  
   // Run tweego
   runTweego();
+  
+  // Completed
+  spinner.succeed(`Build completed.`)
 })();
--- .build/tweego_installer.js
+++ .build/tweego_installer.js
@@ -1,6 +1,8 @@
 import decompress from "decompress";
 import fs from "fs";
-import del from "del";
+import { deleteAsync } from "del";
+import ora from "ora";
+import chalk from 'chalk';
 import got from "got";
 import stream from "stream";
 import { config } from "./build_config.js";
@@ -8,43 +10,53 @@ import { promisify } from "util";
 
 const pipeline = promisify(stream.pipeline);
 
+const getInstallSpinner = () => {
+  return ora({ prefixText: chalk.gray(`[installing]`)});
+}
+
 export class Installer {
   static async downloadAndExtract(link, to) {
     const filePath = link.split("/").pop();
 
     // Ensure clean entry
-    await del(filePath);
+    await deleteAsync(filePath);
     // Download
     await pipeline(got.stream(link), fs.createWriteStream(filePath));
     // Unzip
     await decompress(filePath, to);
     // Cleanup
-    await del(filePath);
+    await deleteAsync(filePath);
   }
 
   static async installTweego() {
-    console.log(
-      `[installer] Installing Tweego (${config.tweego.binaries.version}) for ${process.platform} ${process.arch}`
-    );
-    const zipLink = Installer.getTweegoZipLink();
-    await Installer.downloadAndExtract(zipLink, config.tweego.dir);
+    const spinner = getInstallSpinner(true);
+    spinner.start(`Tweego (${config.tweego.binaries.version}) for ${process.platform}-${process.arch}`);
+
+    try {
+      const zipLink = Installer.getTweegoZipLink();
+      await Installer.downloadAndExtract(zipLink, config.tweego.dir);
+      spinner.succeed();
+    } catch (e) {
+      spinner.fail(`Failed to install Tweego.\n\n${e}`);
+    }
   }
 
   static async installSugarCubeStoryFormat() {
-    console.log(
-      `[installer] Installing SugarCube (${config.tweego.storyFormats.sugarcube.version}) StoryFormat`
-    );
-    const zipLink = config.tweego.storyFormats.sugarcube.link;
-    await Installer.downloadAndExtract(
-      zipLink,
-      [config.tweego.dir, "storyformats"].join("/")
-    );
+    const spinner = getInstallSpinner(true);
+    spinner.start(`SugarCube StoryFormat (${config.tweego.storyFormats.sugarcube.version})`);
+    try {
+      const zipLink = config.tweego.storyFormats.sugarcube.link;
+      const extractTo = [config.tweego.dir, "storyformats"].join("/")
+      await Installer.downloadAndExtract(zipLink, extractTo);
+      spinner.succeed();
+    } catch (e) {
+      spinner.fail(`Failed to install SugarCube StoryFormat.\n\n${e}`);
+    }
   }
 
   static async install() {
     await Installer.installTweego();
     await Installer.installSugarCubeStoryFormat();
-    console.log(``);
   }
 
   static getTweegoZipLink() {
